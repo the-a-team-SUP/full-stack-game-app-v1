524bea509701121f9e6922e9d5e6543b53e65edd
bihire <habimanaolivier6@gmail.com>
2020-03-04 17:47:32 +0200

    ft(done-with-login-page-#171625345):As owner of this task i have created login page which will allow user to interact with facebook login button [finishes #171625345]

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -10,7 +10,7 @@ class Login extends Component {
                         <img alt="Facebook Logo" src={logo} className="logo" />
                         <h2 className="highlight">full-stack-game-app-v1</h2>
                         <p>
-                            full-stack-game-app-v1 is a coolest game ever !! This Multiple choice or objective response is a form of an objective assessment in which respondents are asked to select only correct answers from the choices offered as a list. The multiple choice format is most frequently used in educational testing, in market research, and in elections, when a person chooses between multiple candidates or parties.
+                            full-stack-game-app-v1 is a coolest game ever !! This is a coolest Multiple choice game or objective response is a form of an objective assessment in which respondents are asked to select only correct answers from the choices offered as a list. The multiple choice format is most frequently used in educational testing, in market research, and in elections, when a person chooses between multiple candidates or parties.
 						</p>
                         <a ><button className="button">Login With Facebook</button></a>
                     </div>
