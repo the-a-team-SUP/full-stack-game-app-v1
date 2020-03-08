bb0a7c14c6030be0b0ae3f30404218ffa3288f11
Ishoborabyose <habimanaolivier6@gmail.com>
2020-03-08 10:13:26 +0200

    ft(retrieve scores): [finishes]

--- client/src/Components/Landing.js
+++ client/src/Components/Landing.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { connect } from 'react-redux';
 import { fetchQuestionRequest } from '../redux';
 import QuestionList from '../Components/Questions/qestionsList';
+import ScoreList from '../Components/GameScore/scoresList';
 import OnUsers from './OnUsers'
 import Logout from './Logout';
 
@@ -12,7 +13,23 @@ class Landing extends Component {
 
   render() {
     const { questions } = this.props.question;
-
+    const { users } = this.props.selectedGame;
+    const loggedInUsers = this.props.loggedInUsers;
+    const { isGameOpen } = this.props.isGameOpen;
+    console.log( "======== -----===" )
+    console.log( isGameOpen )
+    
+    console.log( "======== -----===>" )
+    let result;
+    if ( isGameOpen === "done" )
+    {
+    result=  <ScoreList scoreList={users} usersList={loggedInUsers} />;
+    }
+    else
+    {
+      result= <QuestionList questions={ questions } />
+    }
+    
     return (
       <div className="wrapper">
         <div>
@@ -21,18 +38,16 @@ class Landing extends Component {
           <br></br>
         </div>
         <div className="container">
-          <div class="userSide">
-            <Logout history={this.props.history} />
-            <img src="https://scontent.fkgl3-1.fna.fbcdn.net/v/t31.0-8/p960x960/18595201_1388101917942882_8338415819528657694_o.jpg?_nc_cat=111&_nc_sid=85a577&_nc_eui2=AeGHMCbPiqtJPHzN2POLLaN-oiKJ8uSwlDLn4DPz1WKGRfqzIQvLFiUHN4DZnLLPddENRgJSb9w9ePc13SEyXDG2mfruTR9H0mXN8r38Hxo_7A&_nc_ohc=4ILfuPtH8EkAX-V2rcm&_nc_pt=5&_nc_ht=scontent.fkgl3-1.fna&_nc_tp=6&oh=db0d3a586f50de626dd59959e22da0a1&oe=5E97FB61"></img>
-            <b><p>Guevara</p></b>
-            <p>manziguevara@gmail.com</p>
-            <hr></hr>
-            <h2>Game</h2>
-            <div>
-              <QuestionList questions={questions} />
-            </div>
-          </div>
-        </div>
+		<div className="userSide">
+		<img src="https://scontent.fkgl3-1.fna.fbcdn.net/v/t31.0-8/p960x960/18595201_1388101917942882_8338415819528657694_o.jpg?_nc_cat=111&_nc_sid=85a577&_nc_eui2=AeGHMCbPiqtJPHzN2POLLaN-oiKJ8uSwlDLn4DPz1WKGRfqzIQvLFiUHN4DZnLLPddENRgJSb9w9ePc13SEyXDG2mfruTR9H0mXN8r38Hxo_7A&_nc_ohc=4ILfuPtH8EkAX-V2rcm&_nc_pt=5&_nc_ht=scontent.fkgl3-1.fna&_nc_tp=6&oh=db0d3a586f50de626dd59959e22da0a1&oe=5E97FB61" alt="profile" />
+		<b><p>Guevara</p></b>
+		<p>manziguevara@gmail.com</p>
+		<hr></hr>
+		<div>
+      {result}
+		</div>
+	</div>
+	</div>
       </div>
     );
   }
@@ -45,7 +60,12 @@ const mapActionToProps = dispatch => {
 }
 
 const mapStateToProps = (state) => {
-  return { question: state.question }
+  return {
+    question: state.question,
+    selectedGame: state.game.selectedGame,
+    loggedInUsers: state.user.loggedInUsers,
+    isGameOpen: state.game
+  }
 };
 
 export default connect(mapStateToProps, mapActionToProps)(Landing);
--- client/src/redux/user/userReducer.js
+++ client/src/redux/user/userReducer.js
@@ -2,7 +2,38 @@ import { LOGIN_USER, LOGOUT_USER } from "./userTypes";
 import axios from 'axios';
 
 const initialStatus = {
-  loggedInUsers: []
+  loggedInUsers: [
+    {
+      name: 'Izabayo',
+      email: "izabayo@stackup.com",
+      picture: 'https://scontent-jnb1-1.xx.fbcdn.net/v/t1.0-9/11008804_813019598783174_5403540366515761916_n.jpg?_nc_cat=111&_nc_sid=7aed08&_nc_eui2=AeHFCfn4a-DcI6Qr_wjUnauBfFhWIqw0Ga2cuMlneuUhOG-M6QPMBO2fP31P-flAJ7lTe2KowHeqO237IzE89lNOeyGFN-G7lbBJpZNuD55eqg&_nc_ohc=D8b_3dZv95MAX_0AQPV&_nc_ht=scontent-jnb1-1.xx&oh=91a8872926ff6244e38979b47e9ddb11&oe=5E982E15',
+      userID: 1
+    },
+    {
+      name: 'Doddy',
+      email: "dody@stackup.com",
+      picture: 'https://cdn.cnn.com/cnnnext/dam/assets/191114120109-dog-aging-project-1-super-tease.jpg',
+      userID: 2
+    },
+    {
+      name: 'christian',
+      email: "christian@stackup.com",
+      picture: 'https://img.huffingtonpost.com/asset/5dcc613f1f00009304dee539.jpeg?cache=QaTFuOj2IM&ops=crop_834_777_4651_2994%2Cscalefit_720_noupscale&format=webp',
+      userID: 3
+    },
+    {
+      name: 'Karen Giramata',
+      email: "karen@stackup.com",
+      picture: 'https://scontent-jnb1-1.xx.fbcdn.net/v/t1.0-9/282908_481889085208631_1454687981_n.jpg?_nc_cat=104&_nc_sid=210fed&_nc_eui2=AeGk_zgxzj5qWWqDQPB-KN5RjA6NqpvxDHEqMNEzzSdyNuQZXMtddSyIaaaGa2N3P4i5vbau63fFPyW36Q2SIcFLxyi7RGYuJAqeiSKYdcjDEw&_nc_ohc=_F9zaovc0wQAX9xM4PT&_nc_ht=scontent-jnb1-1.xx&oh=7a8facdb65c006ddbbf8525cb2907b83&oe=5E90AB10',
+      userID: 4
+    },
+    {
+      name: 'Manzi Guevara',
+      email: "guevar@stackup.com",
+      picture: 'https://scontent-jnb1-1.xx.fbcdn.net/v/t1.0-9/1470187_545513135535102_539884773_n.jpg?_nc_cat=107&_nc_sid=05277f&_nc_eui2=AeEbFax_OoF1imf6bpmwg5GHe8NndJcHShgIx_uxOOne_-N6hSQncXBSx60bocBZJazHBoyXSOXHzWo9uZY3U6090yI4NobzCDaoTfhobfk6PA&_nc_ohc=6uKuA5IhvYIAX96avUz&_nc_ht=scontent-jnb1-1.xx&oh=fe52ad8346560d30b3cb55f2b9122ff0&oe=5E95208D',
+      userID: 5
+    }
+  ]
 };
 
 const reducer = (state = initialStatus, action) => {
