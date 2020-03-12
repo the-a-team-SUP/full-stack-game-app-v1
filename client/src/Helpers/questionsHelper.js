7631a40e8cff77672208d38458fdf00fbd193eb5
jajajaden01 <jajajaden01@gmail.com>
2020-03-12 14:32:47 +0200

    ft-fixing-bugs

--- client/src/Helpers/questionsHelper.js
+++
@@ -1,3 +0,0 @@
-export const getQuestionsIds = (questions) => {
-    return questions.map((data) => data.id);
-}
--- client/src/redux/index.js
+++
@@ -1,3 +0,0 @@
-export * from './user/userActions'
-export * from './question/questionActions'
-export * from './game/gameActions'
